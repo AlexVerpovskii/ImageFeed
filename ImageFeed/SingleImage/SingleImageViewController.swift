5672f2ec2bbcfba9fd35488149170430690b8a79
AlexVerpovskii <alex.verpovskii@gmail.com>
2024-05-26 16:12:05 +0300

    Homework sprint 0

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Верповский on 18.05.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            photoImageView.image = image
+        }
+    }
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet private var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.125
+        photoImageView.image = image
+        photoImageView.frame.size = image!.size
+        
+        rescaleAndCenterImageInScrollView(image: image!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func backButtonAction(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        photoImageView
+    }
+}
